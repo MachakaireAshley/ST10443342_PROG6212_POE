d577b13d907d3dc7ccedaf36e95ddb947b9b0308
MachakaireAshley <ST10443342@imconnect.edu.za>
2025-11-18 02:01:31 +0200

    Eighth Commit
    This commit shows some views having been created

--- CMCS/obj/Debug/net9.0/CMCS.AssemblyInfo.cs
+++ CMCS/obj/Debug/net9.0/CMCS.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CMCS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+64862c9479c2523373dc5ec02148216e9281bfc4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+15cd12e61569a2e660c7dea98a63a0a491e60068")]
 [assembly: System.Reflection.AssemblyProductAttribute("CMCS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CMCS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
